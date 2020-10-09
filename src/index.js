9a5b756391e6a78602665ba093f2ea75ca78d261
Maksim Bashchuk <MaksimBashchuk@gmail.com>
2020-10-09 17:28:21 +0300

    feat: add human-readable-number solution

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 module.exports = function toReadable (number) {
-  let numbers = new Map (
-    [0, 'zero'], [1, 'one'], [2, 'two'], [3, 'three'], [4, 'four'], [5, 'five'], [6, 'six'], 
-    [7, 'seven'], [8, 'eight'], [9, 'nine'], [10, 'ten'], [11, 'eleven'], [12, 'twelve'], 
-    [13, 'thirteen'], [15, 'fifteen'], [20, 'twenty'], [30, 'thirty'], [50, 'fifty'], [80, 'eighty']
-  );
-  if ()
-  const newNumber = number.toString().split('').forEach(i => +i);
-  let result = '';
-
+  const nums = new Map([['1', 'one'], ['2', 'two'], ['3', 'three'], ['4', 'four'], ['5', 'five'], ['6', 'six'], ['7', 'seven'], 
+    ['8', 'eight'], ['9', 'nine'], ['10', 'ten'], ['11', 'eleven'], ['12', 'twelve'], ['13', 'thirteen'], ['14', 'fourteen'], ['15', 'fifteen'], 
+    ['16','sixteen'], ['17','seventeen'], ['18','eighteen'], ['19','nineteen'], ['20', 'twenty'], ['30', 'thirty'], 
+    ['40','forty'], ['50', 'fifty'], ['60','sixty'], ['70','seventy'], ['80', 'eighty'], ['90','ninety']]);
+  if (number === 0) return 'zero';
+  let arrNumber = [];
+  arrNumber[0] = parseInt(number / 100).toString();
+  arrNumber[1] = (parseInt(number / 10) % 10).toString();
+  arrNumber[2] = number.toString().slice(-1);
+  let result ='';
   
+  result += arrNumber[0] !== '0' ? `${nums.get(arrNumber[0])} hundred` : '';
+  result += arrNumber[1] !== '0' && arrNumber[1] !== '1' ? ` ${nums.get(arrNumber[1] + 0)}` : '';
+  result += arrNumber[1] === '1' && arrNumber[2] !== '0' ? ` ${nums.get(arrNumber[1] + arrNumber[2])}` : '';
+  result += arrNumber[1] === '1' && arrNumber[2] === '0' ? ` ${nums.get(arrNumber[1] + arrNumber[2])}` : '';
+  result += arrNumber[2] !== '0' && arrNumber[1] !== '0' && arrNumber[1] !== '1' ? ` ${nums.get(arrNumber[2])}` : '';
+  result += arrNumber[2] !== '0' && arrNumber[1] === '0' ? ` ${nums.get(arrNumber[2])}` : '';
 
-  if (newNumber.length === 3) {
-    result = `${numbers.get(newNumber[0])} hundred`;
-  }
+  return result.trim();
 };
